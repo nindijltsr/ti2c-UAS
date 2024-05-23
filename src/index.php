82674029fa8278cf67a79b86f0237ebcb1c7e8d8
ACER-027 <laptopindo28@gmail.com>
2024-05-23 15:33:07 +0700

    update

--- src/index.php
+++ src/index.php
@@ -3,9 +3,15 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document </title>
+  <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+  <title>FoodGet</title>
+
+  
 </head>
 <body>
-  <h1>OKE</h1>
+  <?php include '../assets-templates/header.html'; ?>
+
 </body>
+
+<script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </html>
