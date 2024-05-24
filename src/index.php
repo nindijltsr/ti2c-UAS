bd5cd110f7f070e9289618ae9090c85fc1f495e1
ACER-027 <laptopindo28@gmail.com>
2024-05-24 14:26:01 +0700

    update

--- src/index.php
+++ src/index.php
@@ -5,13 +5,79 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
   <title>FoodGet</title>
+  <style>
+    * {
+      padding: 0;
+      margin: 0;
+    }
 
+    /* SLIDE */
+    .carousel-item {
+      height: 606px;
+      width: 100%;
+    }
+    .carousel-item img {
+      margin-top: -190px;
+      padding-top: 130px;
+    }
+    .slide {
+      width: 100%;
+    }
+    
+    .font{
+        color: white;
+        font-size: clamp(16px, 2vw, 20px);
+    }
+    @media(max-width:768px) {
+      .photo-gallery{
+        flex-direction: column;
+      }
+    }
+  </style>
   
 </head>
 <body>
   <h1>HALLO NIN</h1>
   <?php include '../assets-templates/header.html'; ?>
 
+  <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="../assets-templates/img/Slide1.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block">
+          <button type="button" class="btn btn-danger">Pesan Sekarang</button>
+          <button type="button" class="btn btn-secondary">Jelajahi Menu</button>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="../assets-templates/img/Slide2.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption  d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="../assets-templates/img/Slide3.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 </body>
 
 <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
