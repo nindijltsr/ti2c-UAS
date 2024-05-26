e5c9b31ac16353a61b9b561ecaca8b16d049308a
ACER-027 <laptopindo28@gmail.com>
2024-05-26 22:46:32 +0700

    update

---
+++ src/listMinuman.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Get</title>
+
+    <style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    p {
+        font-weight: bold;
+    }
+    </style>
+</head>
+<body>
+    <?php include '../assets-templates/header.html'; ?>
+
+    <div class="container mt-5">
+    <h2 class="mb-4">Minuman</h2>
+    <div class="d-flex flex-wrap justify-content-between">
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/minuman/esteh.png" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Nasi Goreng</h4>
+                <p class="card-text mt-1">Rp 12.000,00</p>
+                <a href="#" class="btn btn-primary align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/minuman/esjeruk.png" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Nasi Goreng</h4>
+                <p class="card-text mt-1">Some quick example.</p>
+                <a href="#" class="btn btn-primary  align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/minuman/tehtarik.png" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Nasi Goreng</h4>
+                <p class="card-text mt-1">Some quick example.</p>
+                <a href="#" class="btn btn-primary align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/minuman/cappucino.png" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Nasi Goreng</h4>
+                <p class="card-text mt-1">Some quick example.</p>
+                <a href="#" class="btn btn-primary align-self-end">Pesan</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
