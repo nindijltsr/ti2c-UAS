cfd6bb5cf3aa534f346c30132abc9f029f695492
LAPTOP-UPG5JS3M\User <anindyaputri231204@gmail.com>
2024-05-27 17:25:48 +0700

    update

--- src/promo.php
+++ src/promo.php
@@ -21,8 +21,8 @@
         .container {
             text-align: center;
             width: calc(33.33% - 40px);
-            margin: 5px;
-            padding: 100px;
+            margin: 8px;
+            padding: 130px;
             background-color: #BB0A13;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -54,7 +54,7 @@
         }
 
         .container .btn {
-            padding: 5px 10px;
+            padding: 7px 10px;
             background-color: #969A36;
             color: white;
             border: none;
@@ -74,54 +74,54 @@
 
         /* Background images */
         .container:nth-child(1) {
-            background-image: url("/assets-templates/img/promo/prm1.png");
+            background-image: url("../assets-templates/img/promo/promo1.png");
         }
 
         .container:nth-child(2) {
-            background-image: url("./assets-templates/img/promo/promo2.png");
+            background-image: url("../assets-templates/img/promo/promo2.png");
         }
 
         .container:nth-child(3) {
-            background-image: url("./assets-templates/img/promo/promo3.png");
+            background-image: url("../assets-templates/img/promo/promo3.png");
         }
 
         .container:nth-child(4) {
-            background-image: url("./assets-templates/img/promo/promo4.png");
+            background-image: url("../assets-templates/img/promo/promo4.png");
         }
 
         .container:nth-child(5) {
-            background-image: url("./assets-templates/img/promo/promo5.png");
+            background-image: url("../assets-templates/img/promo/promo5.png");
         }
 
         .container:nth-child(6) {
-            background-image: url("./assets-templates/img/promo/promo6.png");
+            background-image: url("../assets-templates/img/promo/promo6.png");
         }
     </style>
 </head>
 <body>
     <div class="container" data-promo="10%">
         <p>Diskon 10% untuk menu steak</p>
-        <a href="menu_makanan.html?promo=st10" class="btn">Pakai Diskon 10%</a>
+        <a href="listMakanan.php" class="btn">Pakai Diskon 10%</a>
     </div>
     <div class="container" data-promo="Buy 2 Get 1">
         <p>Beli 2 Gratis 1 untuk menu nasi goreng</p>
-        <a href="menu_makanan.html?promo=nasgor21" class="btn">Pakai Beli 2 Gratis 1</a>
+        <a href="listMakanan.php?promo=nasgor21" class="btn">Pakai Beli 2 Gratis 1</a>
     </div>
     <div class="container" data-promo="5%">
         <p>Diskon 5% untuk menu mojito</p>
-        <a href="menu_minuman.html?promo=mjt5" class="btn">Pakai Diskon 5%</a>
+        <a href="listMakanan.php?promo=mjt5" class="btn">Pakai Diskon 5%</a>
     </div>
     <div class="container" data-promo="5%">
         <p>Diskon 5% untuk menu jus</p>
-        <a href="menu_minuman.html?promo=js5" class="btn">Pakai Diskon 5%</a>
+        <a href="listMakanan.php?promo=js5" class="btn">Pakai Diskon 5%</a>
     </div>
     <div class="container" data-promo="Buy 1 Get 1">
         <p>Beli 1 Gratis 1 untuk menu kentang</p>
-        <a href="menu_makanan.html?promo=ktg11" class="btn">Pakai Beli 1 Gratis 1</a>
+        <a href="listMakanan.php?promo=ktg11" class="btn">Pakai Beli 1 Gratis 1</a>
     </div>
     <div class="container" data-promo="Buy 1 Get 1">
         <p>Beli 1 Gratis 1 untuk menu americano</p>
-        <a href="menu_minuman.html?promo=amrc11" class="btn">Pakai Beli 1 Gratis 1</a>
+        <a href="listMakanan.php?promo=amrc11" class="btn">Pakai Beli 1 Gratis 1</a>
     </div>
     <script src="./vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
