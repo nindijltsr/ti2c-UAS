d5d7dfd041b9f441d1dfad9fb53f136aefcb65fe
ACER-027 <laptopindo28@gmail.com>
2024-05-27 19:21:12 +0700

    update

--- src/listMakanan.php
+++ src/listMakanan.php
@@ -31,65 +31,65 @@
     <div class="d-flex flex-wrap justify-content-between">
         <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
             <div class="card-body d-flex flex-column align-items-center">
-                <img src="../assets-templates/img/makanan/nasgor.jpg" class="card-img-top mb-2" alt="...">
-                <h4 class="card-text mb-0">Nasi Goreng</h4>
-                <p class="card-text mt-1">Rp 12.000,00</p>
+                <img src="../assets-templates/img/makanan/makananRingan/cireng isi.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Cireng</h4>
+                <p class="card-text mt-1">Rp 5.000,00 / isi 3</p>
                 <a href="#" class="btn btn-danger align-self-end">Pesan</a>
             </div>
         </div>
         <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
             <div class="card-body d-flex flex-column align-items-center">
-                <img src="../assets-templates/img/makanan/miegoreng.jpg" class="card-img-top mb-2" alt="...">
-                <h4 class="card-text mb-0">Mie Goreng</h4>
-                <p class="card-text mt-1">Rp 14.000,00</p>
+                <img src="../assets-templates/img/makanan/makananRingan/dimsum.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Dimsum</h4>
+                <p class="card-text mt-1">Rp 10.000,00/ isi 3</p>
                 <a href="#" class="btn btn-danger  align-self-end">Pesan</a>
             </div>
         </div>
         <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
             <div class="card-body d-flex flex-column align-items-center">
-                <img src="../assets-templates/img/makanan/bakso.jpg" class="card-img-top mb-2" alt="...">
-                <h4 class="card-text mb-0">Bakso</h4>
+                <img src="../assets-templates/img/makanan/makananRingan/heci.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Bakwan Sayur</h4>
+                <p class="card-text mt-1">Rp 2.000,00 / 1 biji</p>
+                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananRingan/kentang.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Kentang Goreng</h4>
                 <p class="card-text mt-1">Rp 12.000,00</p>
                 <a href="#" class="btn btn-danger align-self-end">Pesan</a>
             </div>
         </div>
         <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
             <div class="card-body d-flex flex-column align-items-center">
-                <img src="../assets-templates/img/makanan/soto.jpg" class="card-img-top mb-2" alt="...">
-                <h4 class="card-text mb-0">Soto Kudus</h4>
-                <p class="card-text mt-1">Rp 15.000,00</p>
+                <img src="../assets-templates/img/makanan/makananRingan/risol mayo.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Risol Mayo</h4>
+                <p class="card-text mt-1">RP 7.000,00 / isi 3</p>
                 <a href="#" class="btn btn-danger align-self-end">Pesan</a>
             </div>
         </div>
         <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
             <div class="card-body d-flex flex-column align-items-center">
-                <img src="../assets-templates/img/makanan/rawon.jpg" class="card-img-top mb-2" alt="...">
-                <h4 class="card-text mb-0">Rawon</h4>
-                <p class="card-text mt-1">RP 15.000,00</p>
+                <img src="../assets-templates/img/makanan/makananRingan/Samose❤️.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Samose</h4>
+                <p class="card-text mt-1">Rp 8.000,00 / isi 5</p>
                 <a href="#" class="btn btn-danger align-self-end">Pesan</a>
             </div>
         </div>
         <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
             <div class="card-body d-flex flex-column align-items-center">
-                <img src="../assets-templates/img/makanan/ayamgoreng.jpg" class="card-img-top mb-2" alt="...">
-                <h4 class="card-text mb-0">Ayam Goreng</h4>
-                <p class="card-text mt-1">Rp 11.000,00</p>
-                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
-            </div>
-        </div>
-        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
-            <div class="card-body d-flex flex-column align-items-center">
-                <img src="../assets-templates/img/makanan/ayambakar.jpg" class="card-img-top mb-2" alt="...">
-                <h4 class="card-text mb-0">Ayam Bakar</h4>
-                <p class="card-text mt-1">Rp 15.000,00</p>
+                <img src="../assets-templates/img/makanan/makananRingan/tahu isi.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Tahu isi</h4>
+                <p class="card-text mt-1">Rp 5.000,00 / isi 3</p>
                 <a href="#" class="btn btn-danger align-self-end">Pesan</a>
             </div>
         </div>
         <div class="card mb-3" style="width: 15rem; background-color:whitesmoke;">
             <div class="card-body d-flex flex-column align-items-center">
                 <img src="../assets-templates/img/makanan/ayamgeprek.jpg" class="card-img-top mb-2" alt="...">
-                <h4 class="card-text mb-0">Ayam Geprek</h4>
-                <p class="card-text mt-1">Rp 10.000,00</p>
+                <h4 class="card-text mb-0">Tempe Goreng</h4>
+                <p class="card-text mt-1">Rp 5.000,00 / isi 3</p>
                 <a href="#" class="btn btn-danger align-self-end">Pesan</a>
             </div>
         </div>
@@ -99,7 +99,7 @@
     <div class="d-flex flex-wrap justify-content-between">
         <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
             <div class="card-body d-flex flex-column align-items-center">
-                <img src="../assets-templates/img/makanan/nasgor.jpg" class="card-img-top mb-2" alt="...">
+                <img src="../assets-templates/img/makanan/makananBerat/nasgor.jpg" class="card-img-top mb-2" alt="...">
                 <h4 class="card-text mb-0">Nasi Goreng</h4>
                 <p class="card-text mt-1">Rp 12.000,00</p>
                 <a href="#" class="btn btn-danger align-self-end">Pesan</a>
@@ -194,6 +194,75 @@
             </div>
         </div>
     </div>
+
+    <h2 class="mb-4" style="color:black; font-size:22px;">Makanan Penutup</h2>
+    <div class="d-flex flex-wrap justify-content-between">
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananPenutup/brownies.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Brownies</h4>
+                <p class="card-text mt-1">RP 12.000,00</p>
+                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananPenutup/cheese cake.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Cheese Cake</h4>
+                <p class="card-text mt-1">Rp 20.000,00</p>
+                <a href="#" class="btn btn-danger  align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananPenutup/churros.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Churros</h4>
+                <p class="card-text mt-1">Rp 12.000,00</p>
+                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananPenutup/lava cake.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Lava Cake</h4>
+                <p class="card-text mt-1">Rp 20.000,00</p>
+                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananPenutup/macarons.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Macarons</h4>
+                <p class="card-text mt-1">Rp 18.000,00</p>
+                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananPenutup/panna cota.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Panna Cota</h4>
+                <p class="card-text mt-1">Rp 18.000,00</p>
+                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-4" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananPenutup/waffle.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Waffle</h4>
+                <p class="card-text mt-1">Rp 12.000,00</p>
+                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
+            </div>
+        </div>
+        <div class="card mb-3" style="width: 15rem; background-color:whitesmoke;">
+            <div class="card-body d-flex flex-column align-items-center">
+                <img src="../assets-templates/img/makanan/makananPenutup/mochi.jpg" class="card-img-top mb-2" alt="...">
+                <h4 class="card-text mb-0">Mochi Mini</h4>
+                <p class="card-text mt-1">Rp 5.000,00 / isi 3</p>
+                <a href="#" class="btn btn-danger align-self-end">Pesan</a>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
